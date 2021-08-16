fdfce2c6004ce9932eefbeb18bac8b677dfdcf02
PaulB1985 <paulpriest1985@gmail.com>
2021-08-16 21:35:50 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (min.arguments.length > 0) {
+        if (array.length > 0) {
+            return Math.min(...array)
+        } else {
+            return 0
+        }
+    } else {
+        return 0
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (max.arguments.length > 0) {
+        if (array.length > 0) {
+            return Math.max(...array)
+        } else {
+            return 0
+        }
+    } else {
+        return 0
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (avg.arguments.length > 0) {
+        if (array.length > 0) {
+            const sum = array.reduce((sum, current) => sum + current, 0)
+            let avg = sum / array.length
+            return avg
+        } else {
+            return 0
+        }
+    } else {
+        return 0
+    }
 }
